8e68eb57dad8faed3248291529721842fed57130
Joshua Rose <joshuarose@outlook.com>
2018-07-12 17:06:48 -0400

    Add initial tests and documentation to vue skeleton

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,23 @@
   </div>
 </template>
 
+// SCSS or CSS is supported out of the box, use what brings you joy!
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 #nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+	padding: 30px;
+	a {
+		font-weight: bold;
+		color: #2c3e50;
+		&.router-link-exact-active {
+			color: #42b983;
+		}
+	}
 }
 </style>
